e6874f43358e3cc5d283a465ca625d8d8f971f1d
Rafael V. de Oliveira <elrafael123@gmail.com>
2020-11-16 19:25:39 +0000

    Unit Test project

---
+++ Associados/Associados.Test/AssociadoRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using Associados.Data;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associados.Test
+{
+    [TestClass]
+    public class AssociadoRepositoryTest
+    {
+        AssociadoRepository Repo;
+
+        [TestInitialize]
+        public void TestSetup()
+        {
+            AssociadoDbInit db = new AssociadoDbInit();
+            System.Data.Entity.Database.SetInitializer(db);
+            Repo = new AssociadoRepository();
+        }
+
+        [TestMethod]
+        public void IsRepositoryInitializeWithValidNumberOfData()
+        {
+            var result = Repo.List();
+            Assert.IsNotNull(result);
+            var numberOfRecords = result.ToList().Count;
+            Assert.AreEqual(1, numberOfRecords);
+        }
+    }
+}
