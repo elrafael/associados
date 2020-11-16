ee8b85daa2053f3cc0f3fed8a28572159aadbbaf
Rafael V. de Oliveira <elrafael123@gmail.com>
2020-11-16 18:57:30 +0000

    Core Project

---
+++ Associados/Associados.Core/Associado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associados.Core
+{
+    public class Associado
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        public string Apelido { get; set; }
+        public string Email { get; set; }
+
+    }
+}
---
+++ Associados/Associados.Core/IAssociadoRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associados.Core
+{
+    public interface IAssociadoRepository
+    {
+        void Add(Associado a);
+        void Edit(Associado a);
+        void Remove(int Id);
+        IEnumerable<Associado> List();
+        Associado findById(int Id);
+    }
+}
