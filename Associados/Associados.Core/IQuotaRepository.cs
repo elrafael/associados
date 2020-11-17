78a2c1699476314fe2e0d8d5d177f656c3ba962a
Rafael V. de Oliveira <elrafael123@gmail.com>
2020-11-17 13:09:51 +0000

    Quota Model

---
+++ Associados/Associados.Core/IQuotaRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Associados.Core
+{
+    public interface IQuotaRepository
+    {
+        void Add(Quota a);
+        void Edit(Quota a);
+        void Remove(int Id);
+        IEnumerable<Quota> List();
+        Quota findById(int Id);
+    }
+}
